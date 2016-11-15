74275abd1ceb89d303bb4c333cec2a81cda8d796
Joe Shannon <josephshannon@outlook.com>
2016-11-15 11:41:42 -0500

    Changed read_dir. Added an option to return independent objects for each file rather than one collapsed object. Defaults to TRUE to keep older code working.

--- R/ImportFunctions.R
+++ R/ImportFunctions.R
@@ -25,26 +25,57 @@ read_rdb <- function(file){
 } 
 
 # Read directory ----------------------------------------------------------------
-#' Read in a directory of files with identical data formats
+#' Read in a directory of files
 #'
-#' This function reads in all files from a directory.  All files must have the same layout.
-#' @param dir A directory with only data files
+#' This function reads in all files from a directory and stores them as
+#' individual objects or collapses them into a single object.  Use the 'pattern'
+#' argument to specificy a set of files, or a single file type. 
+#' 
+#' If collapse = TRUE then all files must have identical layouts. When collapse
+#' = F the data will automatically be assigned to objects, so no assignment is
+#' necessary.
+#' @param dir A directory that contains your data files, defaults to the working directory
 #' @param pattern A pattern to match filenames as in \code{\link[base]{list.files}}
 #' @param fun The function to use to read in the files, defaults to 
-#' \code{\link[readr]{read_csv}}
-#' @param ... Additaional arguments to pass to the input method
+#' \code{\link[base]{read.csv}}
+#' @param collapse A logical argument, when true a single object is returned,
+#'   when false an object is returned for each file
+#' @param ... Additional arguments to pass to the input method
 #' @export
 #' @examples
 #' read_dir()
 
-read_dir <- function(dir = getwd(), pattern = NULL, fun = readr::read_csv, ...){
-  fileslist <- list.files(dir, pattern = pattern, full.names = T)
+read_dir <- function(dir = getwd(), pattern = NULL, collapse = TRUE, fun = read.csv, ...){
+  
+  if(!all(sapply(list(dir, pattern), is.character))){
+    stop("dir and pattern must be character vectors.")
+  }
+  
+  if(!is.logical(collapse)){
+    stop("collapse must be TRUE or FALSE.")
+  }
+  
+  if(!is.logical(try(is.function(fun), silent = T))){
+    stop("The function specified in fun is not found. Check your spelling or ensure that the necessary library is loaded.")
+  }
+  
+  fileslist <- list.files(dir, 
+                          pattern = pattern, 
+                          full.names = T)
   nFiles <- length(fileslist)
   for(i in 1:nFiles){
     file.i <- fileslist[i]
     import.i <- fun(file.i, ...)
-    if(i != 1) {import_list <- rbind(import_list, import.i)}
-    else {import_list <- import.i}
+    if(collapse){
+      if(i != 1) {
+        import_list <- rbind(import_list, import.i)
+      } else {
+        import_list <- import.i}
+    } else {
+      assign(basename(file.i), 
+             import.i,
+             envir = .GlobalEnv)
+    }
   }
-  return(import_list)
+  if(collapse){return(import_list)}
 }
